5f2a1b96e52faf5ae4c6d32f8ee97972ea5a6ca0
Aishwarya-Arvindh <168478050+Aishwarya-Arvindh@users.noreply.github.com>
2025-03-18 11:53:08 -0400

    Version 1.0

---
+++ Problem set pattern/src/pattern14_1.java
@@ -0,0 +1,20 @@
+import java.sql.SQLOutput;
+
+public class pattern14_1{
+    public static void main(String[]args){
+    int n=8;
+    int count  =1;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            System.out.print(i);
+            System.out.print(" ");
+            count++;
+
+        }
+        System.out.println();
+
+        }
+    }
+
+
+    }
