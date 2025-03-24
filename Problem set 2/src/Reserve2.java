e059abc27079d4655f98240b3dd81ef17fd4eb34
Aishwarya-Arvindh <168478050+Aishwarya-Arvindh@users.noreply.github.com>
2025-03-24 03:08:33 -0400

    Version 1.0

---
+++ Problem set 2/src/Reserve2.java
@@ -0,0 +1,15 @@
+ class Reserve2 {
+    public static void main(String[] args) {
+        int n= 9879;
+        int count = 0;
+        while (n>0) {
+            int ld =n%10;
+            count=count+1;
+            n=n/10;
+        }
+        System.out.println(count);
+    }
+
+}
+
+
